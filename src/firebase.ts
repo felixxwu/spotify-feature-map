733848424b364f6604dd3511557ecb64153a0d8a
Felix Wu <fwu@shipserv.com>
2025-01-27 00:21:11 +0000

    add firebase

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyClgM48pHZw63bWE-4lgWjp0KoeU_URC58',
+  authDomain: 'spotifywithtags.firebaseapp.com',
+  projectId: 'spotifywithtags',
+  storageBucket: 'spotifywithtags.firebasestorage.app',
+  messagingSenderId: '926099001254',
+  appId: '1:926099001254:web:bb24ac45b998b0d4680967',
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
